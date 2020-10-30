a206146d09e8980cd2d9d6c36bb39d5990915237
Noel Nuguid <notedbynoel@icloud.com>
2020-10-30 05:29:03 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 function env_check() {
-      [[ "$AWS_ACCESS_KEY_ID" ]] || { echo -e "\e[31mAWS_ACCESS_KEY_ID does not exist. Please set it\e[0m"; exit 1; }
-      [[ "$AWS_SECRET_ACCESS_KEY" ]] || { echo -e "\e[31mAWS_SECRET_ACCESS_KEY does not exist. Please set it\e[0m"; exit 1; }
-      [[ "$PROJECT_NAME" ]] || { echo -e "\e[31mPROJECT_NAME does not exist. Please set it\e[0m"; exit 1; }
-      [[ "$DOMAIN_NAME" ]] || { echo -e "\e[31mDOMAIN_NAME does not exist. Please set it\e[0m"; exit 1; }
+      [[ "$AWS_ACCESS_KEY_ID" ]] || { echo -e "\e[31mAWS_ACCESS_KEY_ID does not exist. Please set it.\e[0m"; exit 1; }
+      [[ "$AWS_SECRET_ACCESS_KEY" ]] || { echo -e "\e[31mAWS_SECRET_ACCESS_KEY does not exist. Please set it.\e[0m"; exit 1; }
+      [[ "$PROJECT_NAME" ]] || { echo -e "\e[31mPROJECT_NAME does not exist. Please set it.\e[0m"; exit 1; }
+      [[ "$DOMAIN_NAME" ]] || { echo -e "\e[31mDOMAIN_NAME does not exist. Please set it.\e[0m"; exit 1; }
 }
